ef2230509c3e185fd8393b3937abcb5415f04c15
OrionChar <your.email@example.com>
2025-10-02 07:59:24 -0400

    Init shadcn lib

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import "./app.css";
     import ExerciseController from "./three/ExerciseController";
     import ControlPanel from "./components/ControlPanel.svelte";
     import Statistic from "./components/Statistic.svelte";
@@ -6,49 +7,48 @@
     import TrainingHistoryStore from "./db/TrainingHistoryStore";
     import DBInitializer from "./db/DBInitializer";
     import type ITraining from "./models/ITraining";
-
-    const { exerciseController }: { exerciseController: ExerciseController } =
-        $props();
-
+    
+    const { exerciseController }: { exerciseController: ExerciseController } = $props();
     let isTraining = $state(false);
-
+    
     exerciseController.addEventListener("start", onStart);
     exerciseController.addEventListener("finish", onFinish);
     exerciseController.addEventListener("abort", onAbort);
-
+    
     function onStart(): void {
         isTraining = true;
     }
-
+    
     function onFinish(): void {
         isTraining = false;
         alert("Great!");
     }
-
+    
     function onAbort(): void {
         isTraining = false;
     }
-
+    
     //---
-
     const dbInitializer = new DBInitializer();
     const openRequest = dbInitializer.init();
+    
     openRequest.addEventListener("success", onSuccess);
-
+    
     let trainingHistoryStore: TrainingHistoryStore;
     let trainings: ITraining[] = $state([]);
-
+    
     function onSuccess(this: IDBOpenDBRequest, ev: Event) {
         trainingHistoryStore = new TrainingHistoryStore(this.result);
-        getTrainings()
+        getTrainings();
+    
         exerciseController.addEventListener("finish", ({ detail }) => {
             trainingHistoryStore.add(detail.exercise, detail.repetition);
         });
     }
-
+    
     function getTrainings() {
         const request = trainingHistoryStore.getAll();
-
+    
         request.onsuccess = function () {
             if (request.result) {
                 trainings = request.result; // array of books with price=10
