b253147737f83a6b67c7a56d52acb9dd247c784b
OrionChar <your.email@example.com>
2025-10-03 13:54:49 -0400

    Adapt resolution and size of the canvas

--- src/three/initThree.ts
+++ src/three/initThree.ts
@@ -17,26 +17,41 @@ export default function initThree(canvas: HTMLCanvasElement, exercises: IExercis
 	const interior = new Interior(scene, camera, counter)
 	interior.setupScene(mannequin.scene)
 	interior.setupCamera()
-	
+
 	const renderer = new WebGLRenderer({ canvas });
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	
+	renderer.setPixelRatio(window.devicePixelRatio);
+
+	window.addEventListener('resize', () => onResize(camera, renderer));
+
 	const listExercises = new ExercisesIterator(exercises);
 	const player = new Player(mannequin.scene, listExercises.getCurrentExercise().animation);
 	player.play()
-	
+
 	const orbitController = interior.setupOrbitControl(canvas)
 	const delta = new Clock();
-	animate(scene, camera, renderer, orbitController, delta, player);
+	render(scene, camera, renderer, orbitController, delta, player);
 
 	return new ExerciseController(interior, listExercises, player)
 }
 
-function animate(scene: Scene, camera: Camera, renderer: WebGLRenderer, orbitController: OrbitControls, delta: Clock, player: Player): void {
-	requestAnimationFrame(() => animate(scene, camera, renderer, orbitController, delta, player));
+function render(scene: Scene, camera: Camera, renderer: WebGLRenderer, orbitController: OrbitControls, delta: Clock, player: Player): void {
+	requestAnimationFrame(() => render(scene, camera, renderer, orbitController, delta, player));
 
 	orbitController.update();
 	renderer.render(scene, camera);
 
 	player.animationMixer.update(delta.getDelta());
+}
+
+function onResize(camera: PerspectiveCamera, renderer: WebGLRenderer) {
+	const width = window.innerWidth
+	const height = window.innerHeight
+	
+	// Update camera aspect and projection
+	camera.aspect = width / height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer size and pixel ratio
+	renderer.setSize(width, height);
 }
