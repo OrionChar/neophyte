18e43c06f1decbf80e6661795405f0f0e99257a3
OrionChar <your.email@example.com>
2026-02-06 21:44:12 +0300

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,47 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), svelte()],
+    plugins: [
+        tailwindcss(),
+        svelte(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+                globPatterns: [
+                    '**/*.{js,wasm,css,html}',
+                    './fonts/*.json',
+                    './3d-assets/*.glb'
+                ]
+            },
+            devOptions: {
+                enabled: true,
+            },
+            includeAssets: ['logo-apple-touch-icon.png', 'logo.svg'],
+            manifest: {
+                name: 'Neophyte',
+                short_name: 'Neophyte',
+                description: 'The Service of The Biomechanism',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'logo-pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo-pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+        }),
+    ],
     build: {
         target: 'es2022',
     },
